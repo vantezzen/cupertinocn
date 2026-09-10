1df05a5d14bed959c603ed8934161a884e687dc8
Bennett <hello@vantezzen.io>
2026-09-10 16:22:33 +0200

    add pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.BASE_PATH ?? "/cupertinocn",
   resolve: { alias: { "@": new URL("./src", import.meta.url).pathname } },
   plugins: [react(), tailwindcss()],
 })
